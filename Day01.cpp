c70c722a41e20ee81bf13decedb6122d95a4b1a8
Arfatul Hasan Sagor <arfatulhasansagor@gmail.com>
2026-07-23 01:05:33 +0600

    try to add github

--- Day01.cpp
+++ Day01.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main() {
-  cout << "Hello World!";
+  cout << "Hello World! \n";
+  cout << "Have a good Day! \n";
+  cout << "I am learning C++. \n";
+  // This is a single line comment.
+  /* This is a multi-line comment.
+     It can span multiple lines. 
+     in avobe \n for new line or line break.
+     or we can use endl */
   return 0;
 }
